a068ab4c287fc2899204132117bbf261738aa42d
Giuseppe Damis <giuseppe.damis@digiservice-solutions.it>
2024-11-18 14:48:43 +0100

    preprocess and train py file added

---
+++ src/preprocessing/preprocess.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import boto3
+import sagemaker
+
+def preprocess_data(config):
+    """
+    Preprocessa i dati di input e li salva in S3
+    """
+    pass
---
+++ src/training/train.py
@@ -0,0 +1,8 @@
+import sagemaker
+from sagemaker.sklearn import SKLearn
+
+def train_model(config):
+    """
+    Configura e avvia il training job su SageMaker
+    """
+    pass
